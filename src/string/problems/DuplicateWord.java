0c3c45b1da30e81794ed1d9c642ed340d739cb3b
[quaziarafeen] <shamseelmail1@gmail.com>
2021-04-01 19:47:47 -0400

    Final Questions

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -11,25 +11,7 @@ public class DuplicateWord {
          * Also Find the average length of the words.
          */
 
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-        List<String> list = Arrays.asList(st.split(" "));
 
-
-        Set<String> uniqueWords = new HashSet<String>(list);
-        for (String word : uniqueWords) {
-            System.out.println(word + ": " + Collections.frequency(list, word) + " time(s)");
-
-        }
-
-
-        double TotalCharacters;
-        double AverageOfWords;
-
-        TotalCharacters = st.length() - (list.size() - 1);
-        System.out.println("The Total Character of The String st : " + TotalCharacters);
-
-        AverageOfWords = TotalCharacters / list.size();
-        System.out.println("The Average Length Of The Word in a String st: " + AverageOfWords);
     }
 
 }
