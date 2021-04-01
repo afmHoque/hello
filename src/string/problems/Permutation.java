4a55f84ef3f6dbbdc04ca72bebef73c4401f6646
[quaziarafeen] <shamseelmail1@gmail.com>
2021-03-31 21:59:38 -0400

    JavaQuestion

---
+++ src/string/problems/Permutation.java
@@ -0,0 +1,26 @@
+package string.problems;
+
+
+public class Permutation {
+
+    public static void main(String[] args) {
+
+        /*
+         * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
+         * Write Java program to compute all Permutation of a String
+         *
+         */
+        Permutation ("", "ABC");
+    }
+    public static void  Permutation( String string, String permutation){
+        int x = permutation.length();
+        if(x == 0){
+            System.out.println(string);
+        }
+        else {
+            for (int i=0; i<x; i++){
+                Permutation(string + permutation.charAt(i), permutation.substring(0,i) + permutation.substring(i+ 1,x));
+            }
+        }
+    }
+}
